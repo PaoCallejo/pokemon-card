5d467cb5e8fd20d95aaea5c2007db35582613892
Paola Callejo <pcallejo@healclub.com>
2023-10-20 17:06:14 -0400

    refactored Link to use OnClick

--- src/Components/atoms/pokemonCard/index.tsx
+++ src/Components/atoms/pokemonCard/index.tsx
@@ -1,48 +1,54 @@
-import React from 'react';
-import {CardActionArea} from "@mui/material";
+import React from "react";
+import { CardActionArea } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
-import {Pokemon} from "../../../utils/pokemonsApi";
-import {Link} from "react-router-dom";
+import PokemonCardProps from "./interface";
 
-const PokemonCard = ({data}: { data: Pokemon }) => {
-    const urlDetail: string = "details/" + data.id;
-    return (
-        <Link to={urlDetail}>
+const PokemonCard = ({ data, onClickPokemon }: PokemonCardProps) => {
+  const urlDetail: string = "details/" + data.id;
 
-            <Card sx={{
-                maxWidth: 345,
-                marginX: 5,
-                marginY: 2
-            }}>
-                <CardActionArea>
-                    <CardMedia
-                        sx={{backgroundColor: 'lightyellow'}}
-                        component="img"
-                        height="140"
-                        image={data.image}
-                        alt="image not found"
-
-                    />
-                    <CardContent
-                        sx={{backgroundColor: 'lightsalmon'}}
-                    >
-                        <Typography gutterBottom variant="h5" component="div" style={{color: 'gray'}}>
-                            {data.name.toUpperCase()}
-                        </Typography>
-                        <Typography variant="body2" color="white">
-                            Description: {data.description.slice(0, 2).map((ability, i) => (
-                            i === 0 ? ability.ability.name : `, ${ability.ability.name}`
-                        ))}
-                        </Typography>
-
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
-        </Link>
-    );
-}
+  const handleClick = () => {
+    onClickPokemon(urlDetail);
+  };
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        marginX: 5,
+        marginY: 2,
+      }}
+      onClick={handleClick}
+    >
+      <CardActionArea>
+        <CardMedia
+          sx={{ backgroundColor: "lightyellow" }}
+          component="img"
+          height="140"
+          image={data.image}
+          alt="image not found"
+        />
+        <CardContent sx={{ backgroundColor: "lightsalmon" }}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            style={{ color: "gray" }}
+          >
+            {data.name.toUpperCase()}
+          </Typography>
+          <Typography variant="body2" color="white">
+            Description:{" "}
+            {data.description
+              .slice(0, 2)
+              .map((ability, i) =>
+                i === 0 ? ability.ability.name : `, ${ability.ability.name}`,
+              )}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 export default PokemonCard;
--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,82 +1,97 @@
-import React, {useEffect, useState} from "react";
-import Box from '@mui/material/Box';
-import {useParams} from 'react-router-dom';
-import {getPokemonById, Pokemon} from "../utils/pokemonsApi";
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import { useParams } from "react-router-dom";
+import { getPokemonById, Pokemon } from "../utils/pokemonsApi";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const PokemonDetails = () => {
-    const {id} = useParams();
-    const [pokemon, setPokemon] = useState<Pokemon | undefined>();
-    useEffect(() => {
-        if (id) {
-            getPokemonById(id).then((pokemon) => {
-                setPokemon(pokemon);
-            });
-        }
-    }, []);
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState<Pokemon | undefined>();
+  useEffect(() => {
+    if (id) {
+      getPokemonById(id).then((pokemon) => {
+        setPokemon(pokemon);
+      });
+    }
+  }, []);
 
-    return (
+  return (
+    <div>
+      {pokemon && (
+        <Box sx={{ width: "100%" }}>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <div>
+              <h1 style={{ textAlign: "center" }}>Pokemon Card info</h1>
+              <Grid
+                sx={{
+                  backgroundColor: "lightgreen",
+                }}
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <img src={pokemon.sprites["front_default"]} width="25%" />
+              </Grid>
 
-        <div>
-            {pokemon && (
-                <Box sx={{width: '100%'}}>
-                    <Grid
-                        container
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        <div>
-                            <h1 style={{textAlign: 'center'}}>Pokemon Card info</h1>
-                            <Grid sx={{
-                                backgroundColor: 'lightgreen',
+              <Typography
+                variant="h5"
+                color="black"
+                sx={{
+                  backgroundColor: "lightblue",
+                }}
+              >
+                <strong>Name:</strong> {pokemon.name}
+                <p>
+                  <strong>Height:</strong> {pokemon.height}
+                </p>
+                <p>
+                  <strong>Weight:</strong> {pokemon.weight}
+                </p>
+                <div>
+                  <strong>
+                    <p>Abilities:</p>
+                  </strong>
+                  <ul>
+                    {pokemon.abilities &&
+                      pokemon.abilities.map((ability, index) => (
+                        <li>{ability.ability.name}</li>
+                      ))}
+                  </ul>
+                </div>
+              </Typography>
 
-                            }}
-                                  container
-                                  direction="row"
-                                  justifyContent="center"
-                                  alignItems="center"
-                            >
-                                <img src={pokemon.sprites['front_default']} width='25%'/>
-                            </Grid>
-
-                            <Typography variant="h5" color="black" sx={{
-                                backgroundColor: 'lightblue',
-
-                            }}>
-                                <strong>Name:</strong> {pokemon.name}
-                                <p><strong>Height:</strong> {pokemon.height}</p>
-                                <p><strong>Weight:</strong> {pokemon.weight}</p>
-                                <div>
-                                    <strong><p>Abilities:</p></strong>
-                                    <ul>
-                                        {pokemon.abilities && pokemon.abilities.map((ability, index) => (
-                                            <li>{ability.ability.name}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </Typography>
-
-                            <Typography variant="h5" color="black" sx={{
-                                backgroundColor: 'lightblue',
-
-                            }}>
-                                <div>
-                                    {pokemon.sprites && Object.keys(pokemon.sprites).map((positionImage, index) => (
-                                        <img
-                                            src={pokemon.sprites[positionImage] && typeof pokemon.sprites[positionImage] == 'string' && pokemon.sprites[positionImage]}/>
-
-                                    ))}
-
-                                </div>
-                            </Typography>
-                        </div>
-                    </Grid>
-                </Box>
-            )}
-        </div>
-
-    );
+              <Typography
+                variant="h5"
+                color="black"
+                sx={{
+                  backgroundColor: "lightblue",
+                }}
+              >
+                <div>
+                  {pokemon.sprites &&
+                    Object.keys(pokemon.sprites).map((positionImage, index) => (
+                      <img
+                        src={
+                          pokemon.sprites[positionImage] &&
+                          typeof pokemon.sprites[positionImage] == "string" &&
+                          pokemon.sprites[positionImage]
+                        }
+                      />
+                    ))}
+                </div>
+              </Typography>
+            </div>
+          </Grid>
+        </Box>
+      )}
+    </div>
+  );
 };
 export default PokemonDetails;
